7191a9ff7ceb2becad39c92025786b19ff7162bf
Vakiyama <vakiyama@outlook.com>
2025-04-20 20:20:11 -0700

    feat: add migration and model field

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,6 +34,7 @@ class PostController extends Controller
         $post = new Post();
         $post->title = $validatedData['title'];
         $post->content = $validatedData['content'];
+        $post->published = true;
         $post->user_id = Auth::id();
         $post->save();
 
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -26,6 +26,7 @@ class PostFactory extends Factory
             'title' => $this->faker->sentence(6),
             'content' => $this->faker->paragraphs(3, true),
             'user_id' => User::factory(),
+            'published' => true,
             'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
             'updated_at' => function (array $attributes) {
                 return $this->faker->dateTimeBetween($attributes['created_at'], 'now');
